7828474875bc4bc9a30df05daaca95a6d6ae33bd
Frank Palazzolo <frank@avoidspikes.com>
2024-05-02 23:19:10 -0400

    Add script to assemble everything, test.as and fill.as are running - no pygame gfx yet

---
+++ buildall.bat
@@ -0,0 +1,7 @@
+python main.py programs/test.as programs/test.mc
+python main.py programs/fill.as programs/fill.mc
+python main.py programs/movepixel.as programs/movepixel.mc
+python main.py programs/hello.as programs/hello.mc
+
+rem python simulate.py programs/test.mc
+rem python simulate.py programs/fill.mc
